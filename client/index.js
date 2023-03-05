f55c32ddcfef1ef16184c97186f8e266d4bf7b51
rbasa <=>
2023-03-05 12:43:47 -0300

    solved

--- client/index.js
+++ client/index.js
@@ -1,17 +1,39 @@
 const axios = require('axios');
+const { createHash } = require('crypto');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const verifyProof = require('../utils/verifyProof');
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // create the leaves of the Merkle tree by hashing each name in the nice list
+  const leaves = niceList.map(name => createHash('sha256').update(name).digest());
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  // create the Merkle tree
+  const merkleTree = new MerkleTree(leaves);
 
-  console.log({ gift });
+  // get the root of the Merkle tree
+  const root = merkleTree.getRoot();
+
+  // find the proof that our name is in the list
+  const name = "Sidney Kertzmann";
+  const index = niceList.findIndex(n => n === name);
+  if (index === -1) {
+    console.log('Sorry, your name is not on the nice list.');
+    return;
+  }
+  const proof = merkleTree.getProof(index);
+
+  // verify the proof with the server
+  try {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, { name, proof, root });
+    console.log('Gift:', gift);
+    // console.log('Proof:', proof);
+    // console.log('Root:', root);
+  } catch (error) {
+    console.error(error.response.data);
+  }
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,25 +1,43 @@
 const express = require('express');
+const { createHash } = require('crypto');
+const MerkleTree = require('../utils/MerkleTree');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json');
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// create the leaves of the Merkle tree by hashing each name in the nice list
+const leaves = niceList.map(name => createHash('sha256').update(name).digest());
+
+// create the Merkle tree
+const merkleTree = new MerkleTree(leaves);
+
+// get the root of the Merkle tree
+const root = merkleTree.getRoot().toString('hex');
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // create the merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+
+  // get the root
+  const root = merkleTree.getRoot();
+
+  // find the proof that norman block is in the list 
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  console.log(proof)
+  // Verify that the proof is valid
+  const isValidProof= verifyProof(proof, name, root)
+
+  if (isValidProof) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
